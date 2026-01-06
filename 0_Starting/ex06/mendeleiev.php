1590e38e222aac458ad353cf2ba6b4ef3426b8a4
Lilou <lilou.perez@hotmail.com>
2026-01-06 17:00:29 +0100

    Doing the last exercice with mendeleiev

--- 0_Starting/ex06/mendeleiev.php
+++ 0_Starting/ex06/mendeleiev.php
@@ -2,4 +2,42 @@
 
 $file = fopen("ex06.txt");
 
+// $atom;
+// $number;
+// $symbol;
+// $molar;
+
+    
+$html = '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Mendeleiev </title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <table>';
+    for ($period = 1; $period != 7; $period++)
+    {
+        $html .= '<tr>';
+        for ($group = 1; $group != 18; $group++)
+        {
+            $html .= "<td>";
+
+
+
+            
+            $html .= '</td>';
+        }
+        $html .= '</tr>';
+    }
+    $html .= '</table>;
+</body>;
+
+</html>';
+
+file_put_contents("mendeleiev.html", $html);
 ?>
